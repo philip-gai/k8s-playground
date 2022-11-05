09a273933aa8a2349637b2d82c3686f0b6b7ff64
Philip Gai <philip-gai@github.com>
2022-11-05 18:01:45 +0000

    Use argo server ingress IP

--- argo/setup.sh
+++ argo/setup.sh
@@ -15,15 +15,18 @@ password=$(
 echo "ArgoCD password: $password"
 
 # Run Argo CD using port forwarding
-echo "Running Argo CD using port forwarding"
-kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+echo "Changing Argo CD API Server type to LoadBalancer"
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
 # Wait for the port to forward
 sleep 3
 
+ipAddress=$(kubectl -n argocd get service argocd-server -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+echo "ArgoCD IP Address: $ipAddress"
+
 # Login via CLI
 echo "Logging in via CLI"
-argocd login 'localhost:8080' --username admin --password $password --insecure
+argocd login $ipAddress --username admin --password $password --insecure
 
 # Open Argo CD UI
-echo "Open Argo CD UI by navigating to http://localhost:8080 in your browser"
+echo "Open Argo CD UI by navigating to http://$ipAddress in your browser"
