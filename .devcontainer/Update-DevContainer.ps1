40783f6e1780b44eb31e404f0696b3b2f5a08fb8
Philip Gai <philip-gai@github.com>
2022-11-11 06:00:09 +0000

    Add helm repos

--- .devcontainer/Update-DevContainer.ps1
+++ .devcontainer/Update-DevContainer.ps1
@@ -7,3 +7,6 @@ if (!$?) { throw "Failed to install Homebrew" }
 Write-Host "Installing tools"
 bash $PSScriptRoot/install-tools.sh
 if (!$?) { throw "Failed to install tools" }
+
+helm repo add datadog https://helm.datadoghq.com
+helm repo update
